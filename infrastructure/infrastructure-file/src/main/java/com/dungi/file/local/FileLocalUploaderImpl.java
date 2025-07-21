5c10ed5f1a342f2d77663f7ace177ef4df122dc8
diydriller <dhrhd080@naver.com>
2025-07-22 01:09:31 +0900

    fix: local 업로드 로직에서 폴더 생성 메서드 변경

--- infrastructure/infrastructure-file/src/main/java/com/dungi/file/local/FileLocalUploaderImpl.java
+++ infrastructure/infrastructure-file/src/main/java/com/dungi/file/local/FileLocalUploaderImpl.java
@@ -4,10 +4,14 @@ import com.dungi.core.integration.file.FileUploader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -21,15 +25,16 @@ public class FileLocalUploaderImpl implements FileUploader {
 
     public String imageUpload(MultipartFile file) throws IOException {
         String current_date = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
-        File folder = new File(fileUploadPath);
-        folder.mkdirs();
-        String basePath = folder.getAbsolutePath();
 
-        String[] imageFileFlags = file.getOriginalFilename().split("\\.");
-        String imageExt = imageFileFlags[imageFileFlags.length - 1];
+        Path basePath = Paths.get(fileUploadPath);
+        Files.createDirectories(basePath);
 
-        String imagePath = basePath + "/image" + current_date + "." + imageExt;
-        String imageDownUrl = fileDownPath + "image" + current_date + "." + imageExt;
+        String[] originFilenameParts = file.getOriginalFilename().split("\\.");
+        String originFileExt = originFilenameParts[originFilenameParts.length - 1];
+
+        String imageName = "dungi-image" + current_date + "." + originFileExt;
+        String imagePath = basePath.resolve(imageName).toString();
+        String imageDownUrl = fileDownPath + imageName;
 
         File dest = new File(imagePath);
         file.transferTo(dest);

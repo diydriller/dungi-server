3c148f5a46869a82d60ade6cf00c7a8417a502c3
diydriller <dhrhd080@naver.com>
2025-07-22 01:44:35 +0900

    fix: 중복되는 @Component 제거 & @ConditionalOnProperty 사용

--- infrastructure/infrastructure-file/src/main/java/com/dungi/file/s3/S3Config.java
+++ infrastructure/infrastructure-file/src/main/java/com/dungi/file/s3/S3Config.java
@@ -5,13 +5,11 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
-@Component
-@Profile("prod")
+@ConditionalOnProperty(name = "file.kind", havingValue = "s3")
 @Configuration
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")

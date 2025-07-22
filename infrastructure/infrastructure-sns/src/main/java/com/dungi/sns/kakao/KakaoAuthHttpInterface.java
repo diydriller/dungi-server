64c154f3b3946a3ffbf409ce26c190ccbfec3b32
diydriller <dhrhd080@naver.com>
2025-07-22 13:49:37 +0900

    fix: 인터페이스 분리

--- infrastructure/infrastructure-sns/src/main/java/com/dungi/sns/kakao/KakaoAuthHttpInterface.java
+++ infrastructure/infrastructure-sns/src/main/java/com/dungi/sns/kakao/KakaoAuthHttpInterface.java
@@ -1,20 +1,12 @@
 package com.dungi.sns.kakao;
 
-import com.dungi.sns.kakao.dto.KakaoInfoDto;
 import com.dungi.sns.kakao.dto.SnsTokenDto;
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
-public interface KakaoHttpInterface {
-    @GET("/v2/user/me")
-    Call<KakaoInfoDto> getKakaoInfo(
-            @Header("Authorization") String token,
-            @Header("content-type") String type
-    );
-
+public interface KakaoAuthHttpInterface {
     @POST("/oauth/token")
     Call<SnsTokenDto> getKakaoToken(
             @Query("grant_type") String grantType,

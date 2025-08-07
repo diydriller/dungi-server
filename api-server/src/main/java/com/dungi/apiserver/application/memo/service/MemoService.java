ff548a5941bd174dc01b923c1e99feebaef889ec
diydriller <dhrhd080@naver.com>
2025-08-07 10:58:10 +0900

    fix: memo cache 로직 제거

--- api-server/src/main/java/com/dungi/apiserver/application/memo/service/MemoService.java
+++ api-server/src/main/java/com/dungi/apiserver/application/memo/service/MemoService.java
@@ -10,8 +10,6 @@ import com.dungi.core.domain.memo.query.MemoDetail;
 import com.dungi.core.integration.store.memo.MemoStore;
 import com.dungi.core.integration.store.room.RoomStore;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +23,7 @@ public class MemoService {
 
     // 메모 생성 기능
     // 유저가 방에 입장해있는지 확인 - 메모 생성
-    // 메모 생성시 캐시 삭제
     @Transactional
-    @CacheEvict(key = "#roomId", value = "getMemo")
     public void createMemo(CreateMemoDto dto, Long roomId, Long userId) {
         roomStore.getRoomEnteredByUser(userId, roomId);
         var memo = Memo.builder()
@@ -43,9 +39,7 @@ public class MemoService {
 
     // 메모 조회 기능
     // 유저가 방에 입장해있는지 검증 - 메모 조회
-    // 캐시 사용해서 조회
     @Transactional(readOnly = true)
-    @Cacheable(key = "#roomId", value = "getMemo")
     public List<MemoDetail> getMemo(Long roomId, Long userId) {
         roomStore.getRoomEnteredByUser(userId, roomId);
         return memoStore.getAllMemo(userId, roomId);
@@ -53,9 +47,7 @@ public class MemoService {
 
     // 메모 수정 기능
     // 유저가 방에 입장해있는지 검증 - 메모 수정
-    // 메모 수정시 캐시 삭제
     @Transactional
-    @CacheEvict(key = "#roomId", value = "getMemo")
     public void updateMemo(UpdateMemoDto dto, Long roomId, Long userId, Long memoId) {
         roomStore.getRoomEnteredByUser(userId, roomId);
         var memo = memoStore.getMemo(memoId);
@@ -65,9 +57,7 @@ public class MemoService {
 
     // 메모 이동 기능
     // 유저가 방에 입장해있는지 검증 - 메모 이동
-    // 메모 이동시 캐시 삭제
     @Transactional
-    @CacheEvict(key = "#roomId", value = "getMemo")
     public Memo moveMemo(MoveMemoDto dto, Long roomId, Long userId, Long memoId) {
         roomStore.getRoomEnteredByUser(userId, roomId);
         var memo = memoStore.getMemo(memoId);
@@ -77,9 +67,7 @@ public class MemoService {
 
     // 메모 삭제 기능
     // 유저가 방에 입장해있는지 검증 - 메모 삭제
-    // 메모 삭제시 캐시 삭제
     @Transactional
-    @CacheEvict(key = "#roomId", value = "getMemo")
     public void deleteMemo(Long roomId, Long userId, Long memoId) {
         roomStore.getRoomEnteredByUser(userId, roomId);
         var memo = memoStore.getMemo(memoId);
